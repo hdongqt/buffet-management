def657815e93c18a7b88deeeba0eda44631d46a0
hdongqt <hdongqt@gmail.com>
2025-09-03 14:25:05 +0700

    2025-09-03-feat: add layout, notfound page

---
+++ src/components/userFooter/styled.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+import FooterIMG from '@/assets/images/main/customer-bg.jpg'
+import { Flex } from 'antd'
+import { theme } from '@/constants/theme'
+
+const { breakpoints } = theme
+
+const FooterContentStyled = styled(Flex)`
+  background: linear-gradient(rgba(255, 204, 0, 0.912), rgba(255, 204, 0, 0.95)),
+    url(${FooterIMG}) center/cover no-repeat;
+  padding: 80px 60px;
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    flex-direction: column;
+    row-gap: 28px;
+    padding: 80px 20px;
+  }
+
+  .footer-logo img {
+    width: 16rem;
+  }
+
+  .footer-contact {
+    &__title {
+      color: var(--ant-main-secondary-color);
+      margin-bottom: 12px;
+      font-weight: 600;
+      font-size: 2.8rem;
+    }
+
+    ul {
+      list-style: none;
+      line-height: 1.8;
+      li {
+        font-size: 1.6rem;
+      }
+    }
+  }
+`
+
+const FooterCopyrightStyled = styled.div`
+  background: #b22222;
+  color: #fff;
+  text-align: center;
+  padding: 12px 0;
+  font-size: 14px;
+`
+
+export { FooterContentStyled, FooterCopyrightStyled }
--- src/pages/globalPages/notFound/index.jsx
+++ src/pages/globalPages/notFound/index.jsx
@@ -1,15 +1,29 @@
 import { Button, Result } from 'antd'
+import styled from 'styled-components'
+
+const TitleStyled = styled.p`
+  font-size: 1.6rem;
+`
+
+const ButtonStyled = styled(Button)`
+  background-color: var(--ant-main-primary-color);
+  &.ant-btn-primary.ant-btn-color-primary:hover {
+    background-color: var(--ant-main-secondary-color);
+  }
+`
 
 export default function NotFound() {
   return (
     <Result
       status='404'
       title='404'
-      subTitle='Sorry, the page you visited does not exist.'
+      subTitle={
+        <TitleStyled>Sorry, the page you visited does not exist.</TitleStyled>
+      }
       extra={
-        <Button type='primary' href='/'>
+        <ButtonStyled type='primary' href='/' size='large'>
           Back Home
-        </Button>
+        </ButtonStyled>
       }
     />
   )
